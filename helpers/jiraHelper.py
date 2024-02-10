c112d48632e3253a87463b496bad4bdec025a93c
kirill.gorosov <kirillgorosov1996@gmail.com>
2024-02-10 23:38:52 +0300

    Добавил поддержку прокси

--- helpers/jiraHelper.py
+++ helpers/jiraHelper.py
@@ -1,8 +1,14 @@
-from helpers import authHelper
-
 from jira import JIRA
+import urllib3
+from helpers import authHelper, jsonHelper
 
-# Авторизация в Jira
-jira_options = {'server': authHelper.JIRA_URL}
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+jira_options = {
+    'server': authHelper.JIRA_URL,
+    'verify': False,
+    'proxy': jsonHelper.credentials.get('PROXY')
+}
+
 jira = JIRA(options=jira_options, basic_auth=(authHelper.JIRA_USERNAME, authHelper.JIRA_PASSWORD))
-
