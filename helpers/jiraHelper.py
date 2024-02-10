1e46d0faf8f71215c4ae624d58bfe44a52a82481
kirill.gorosov <kirillgorosov1996@gmail.com>
2024-02-10 23:38:52 +0300

    Исправил создание файлов заметок и дополнил логгер

--- helpers/jiraHelper.py
+++ helpers/jiraHelper.py
@@ -1,8 +1,18 @@
-from helpers import authHelper
-
 from jira import JIRA
+import urllib3
+from helpers import authHelper, jsonHelper
 
-# Авторизация в Jira
-jira_options = {'server': authHelper.JIRA_URL}
+# Отключение предупреждений от urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+
+# Убедитесь, что заменили username, password, proxyserver и port на актуальные данные для вашего прокси-сервера
+
+jira_options = {
+    'server': authHelper.JIRA_URL,
+    'verify': False,
+    'proxy': jsonHelper.credentials.get('PROXY')
+}
+
 jira = JIRA(options=jira_options, basic_auth=(authHelper.JIRA_USERNAME, authHelper.JIRA_PASSWORD))
-
